7082b95f93c2514321f62b867a3d05976f6bf659
Darryl Blake <darryl@nikordi.com>
2015-08-03 11:40:01 -0700

    Installed grunt, added Gruntfile.js with some commented out tasks I will need later.

---
+++ Gruntfile.js
@@ -0,0 +1,95 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'src/<%= pkg.name %>.js',
+        dest: 'build/<%= pkg.name %>.min.js'
+      }
+    },
+
+    // cssmin: {
+    //   combine: {
+    //     files: {
+    //       'public/dist/style.min.css': ['public/**/*.css']
+    //     }
+    //   }
+    // },
+
+
+
+    // jshint: {
+    //   files: [
+    //     'public/client/**/*.js'
+    //   ],
+    //   options: {
+    //     force: 'true',
+    //     jshintrc: '.jshintrc',
+    //     ignores: [
+    //       'public/lib/**/*.js',
+    //       'public/dist/**/*.js'
+    //     ]
+    //   }
+    // },
+
+    // nodemon: {
+    //   dev: {
+    //     script: 'server.js'
+    //   }
+    // },
+
+    // concat: {
+    //   options: {
+    //     separator: '\n'
+    //   },
+    //   js_files: {
+    //     files: {
+    //       'public/dist/lib.js': [
+    //         'public/lib/jquery.js',
+    //         'public/lib/underscore.js',
+    //         'public/lib/backbone.js',
+    //         'public/lib/handlebars.js'
+    //       ],
+    //       'public/dist/client.js': ['public/client/**/*.js']
+    //     }
+    //   }
+    // },
+
+
+    // watch: {
+    //   scripts: {
+    //     files: [
+    //       'public/client/**/*.js',
+    //       'public/lib/**/*.js',
+    //     ],
+    //     tasks: [
+    //       'concat',
+    //       'uglify'
+    //     ]
+    //   },
+    //   css: {
+    //     files: 'public/*.css',
+    //     tasks: ['cssmin']
+    //   }
+    // },
+
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-nodemon');
+
+  // Default task(s).
+  grunt.registerTask('default', ['uglify']);
+
+};
