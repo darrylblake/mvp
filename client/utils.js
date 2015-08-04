5952cdbc90b2582f51c98cd3c5f5b073af43541e
Darryl Blake <darryl@nikordi.com>
2015-08-04 13:00:26 -0700

    Changed name of function.

--- client/utils.js
+++ client/utils.js
@@ -1,4 +1,4 @@
-function alphaNumGen(length, chars) {
+function randomAlphaNum(length, chars) {
   chars = chars || '123456789ABCDEFGHJKMNPQRSTUVWXYZ'
   var result = '';
   for (var i = length; i > 0; --i) result += chars[Math.round(Math.random() * (chars.length - 1))];
