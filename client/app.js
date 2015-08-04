8a102b50394b256f15f2b59d74e45ace2cabdefc
Darryl Blake <darryl@nikordi.com>
2015-08-04 12:15:08 -0700

    Moved graph updating code to controller.

--- client/app.js
+++ client/app.js
@@ -1,35 +1,6 @@
 var app = angular.module('ThumbsCheck', ['ngRoute']);
-unique = Math.floor(Math.random() * 999999 + 99999);
+unique = Math.floor(Math.random() * 999999 + 99999); // TODO: Use sessions
 socket = io();
 
 
-$(function() {
-
-  // Create a new line chart object where as first parameter we pass in a selector
-  // that is resolving to our chart container element. The Second parameter
-  // is the actual data object.
-  var chart;
-
-
-  
-  socket.on('serverdata', function(data){
-    $('#position').text(JSON.stringify(data));
-
-    var data = {
-      // A labels array that can contain any sort of values
-      labels: [1,2,3,4,5,6],
-      // Our series array that contains series objects or in this case series data arrays
-      series: [
-        data
-      ]
-    };
-
-    chart = chart || new Chartist.Line('.chart', data);
-
-    chart.update(data);
-    //$('.marker').css({'top': msg[0], 'left': msg[1]});
-  });
-
-
-});
 
--- client/controllers/speak-controller.js
+++ client/controllers/speak-controller.js
@@ -1,5 +1,21 @@
 var app = angular.module('ThumbsCheck');
 
 app.controller('SpeakController', function() {
-  // An $http AJAX call to the server to gather votes...
+
+  $(function() {
+    var chart;
+    socket.on('serverdata', function(data){
+      var data = {
+        // TODO: Labels should represent each audience member
+        labels: [1,2,3,4,5,6],
+        series: [data]
+      };
+      // Create chart if it doesn't yet exist.
+      chart = chart || new Chartist.Line('.chart', data);
+      chart.update(data);
+    });
+  });
+  
 });
+
+
