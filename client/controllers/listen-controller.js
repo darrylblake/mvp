f6ddf6b2a6f187131f2b73e233083d392d5977e8
Darryl Blake <darryl@nikordi.com>
2015-08-03 23:03:45 -0700

    Added socket emit events to controller.

---
+++ client/controllers/listen-controller.js
@@ -0,0 +1,63 @@
+var app = angular.module('ThumbsCheck');
+
+app.controller('ListenController', function() {
+  // An $http AJAX call to the server to gather votes...
+  $('body').on('mousemove', function(e) {
+    if (e.which == 1) {
+      
+      socket.emit('vote', {
+        'user': unique,
+        'vote': e.pageY
+      });
+    };
+  });
+
+  var unique = Math.floor(Math.random() * 999999 + 999999);
+  // Detecting user touch...
+  // document.addEventListener('touchmove', function(e) {
+  //     e.preventDefault();
+  //     var touch = e.touches[0];
+  //     socket.emit('vote', {
+  //       'user': unique,
+  //       'vote': touch.pageY
+  //     });      
+  // }, false);
+
+$('body').on('mousedown touchstart', function(e) {
+
+  var touch = e.originalEvent.touches;
+  //e.preventDefault();
+
+  var pageX = touch ? touch[0].pageX : e.pageX;
+
+    socket.emit('vote', {
+      'user': unique,
+      'vote': ev.originalEvent.pageY
+    });  
+});
+
+  $('body').on('swipe', function(ev) {
+    //ev.preventDefault();
+    var e = ev.originalEvent
+    e.preventDefault();
+    
+    //var touch = e.touches[0];
+    socket.emit('vote', {
+      'user': unique,
+      'vote': ev.originalEvent.pageY
+    });    
+  });
+
+  // $('body').on({ 'touchstart', function(ev){ 
+  // });
+
+
+  $(".votepatch").on("swipe",function(){
+    alert('testing');
+  });
+
+  socket.on('vote', function(data){
+    $('#position').text(JSON.stringify(data));
+    $('.marker').css({'top': data.vote });
+  });
+});
