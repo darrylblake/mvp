3406c84c229781eab2bebceb5c951125ae0bfedb
Darryl Blake <darryl@nikordi.com>
2015-08-04 12:57:35 -0700

    Added a alpha numerical number random number generater.

---
+++ client/utils.js
@@ -0,0 +1,6 @@
+function alphaNumGen(length, chars) {
+  chars = chars || '123456789ABCDEFGHJKMNPQRSTUVWXYZ'
+  var result = '';
+  for (var i = length; i > 0; --i) result += chars[Math.round(Math.random() * (chars.length - 1))];
+  return result;
+}
